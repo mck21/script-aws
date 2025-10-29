62ac2327a1f31068e8dafb79ffea99113dccb941
Marcos <marcospalomero21@gmail.com>
2025-10-29 19:54:13 +0100

    enable public ipv4

--- script.sh
+++ script.sh
@@ -1,14 +1,27 @@
+#!/bin/bash
+
 # Crear una VPC y devolver su ID
 VPC_ID=$(aws ec2 create-vpc \
-    --cidr-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-    --query 'Vpc.VpcId' \
-    --output text)
+  --cidr-block 192.168.0.0/24 \
+  --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
+  --query 'Vpc.VpcId' \
+  --output text)
 
-#Mostrar
-echo $VPC_ID
+echo "VPC creada con ID: $VPC_ID"
 
-#Habilitar DNS en la VPC
+# Habilitar DNS en la VPC
 aws ec2 modify-vpc-attribute \
-    --vpc-id $VPC_ID \
-    --enable-dns-hostnames "{\"Value\":true}"
+  --vpc-id "$VPC_ID" \
+  --enable-dns-hostnames "{\"Value\":true}"
+
+# Crear subred dentro de la VPC
+SUB_ID=$(aws ec2 create-subnet \
+  --vpc-id "$VPC_ID" \
+  --cidr-block 192.168.0.0/28 \
+  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=MiSubred1}]' \
+  --query 'Subnet.SubnetId' \
+  --output text)
+
+echo "Subred creada con ID: $SUB_ID"
+
+aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
