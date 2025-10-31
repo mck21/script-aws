4134b53264629cf08e2c8ca8398f1ceed3138ba2
Marcos <marcospalomero21@gmail.com>
2025-10-31 19:26:11 +0100

    update

--- script.sh
+++ script.sh
@@ -25,3 +25,33 @@ SUB_ID=$(aws ec2 create-subnet \
 echo "Subred creada con ID: $SUB_ID"
 
 aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch
+
+# Crear Security Group
+SG_ID=$(aws ec2 create-security-group \
+  --vpc-id $VPC_ID \
+  --group-name gs-mck \
+  --description "My security group for port 22" \
+  --output text)
+
+echo "Security Group creado con ID: $SG_ID"
+
+# Autorizar el puerto 22 (ssh) para ese SG // esto no le gusta
+aws ec2 authorize-security-group-ingress \
+    --group-id $SG_ID \
+    --protocol tcp \
+    --port 22 \
+    --cidr 0.0.0.0/0
+
+# Crear EC2 (ejemplo 1)
+EC2_ID=$(aws ec2 run-instances \
+    --image-id ami-0360c520857e3138f \
+    --instance-type t3.micro \
+    --key-name vockey \
+    --subnet-id $SUB_ID \
+    --associate-public-ip-address \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=miEc2}]' \
+    --query Instance.InstanceId --output text)
+
+sleep 15
+
+echo "EC2 creada con ID: $EC2_ID"
